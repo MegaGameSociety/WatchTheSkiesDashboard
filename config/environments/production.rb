a90d80857580e06ecd33d5863f10d604b8379258
Kevin Chang <kevin.w.chang@gmail.com>
2015-10-08 22:11:52 -0500

    Really update to central time

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.time_zone = 'Pacific Time (US & Canada)'
+  config.time_zone = 'Central Time (US & Canada)'
   
 end
