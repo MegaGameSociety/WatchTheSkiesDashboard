62bfe60e4ebd7e1e53ec7a2a7912488922120943
Kevin Chang <kevin.w.chang@gmail.com>
2015-10-27 22:47:01 -0400

    Change time back to Eastern plus fix country status -2 typo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,5 +34,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.time_zone = 'Central Time (US & Canada)'
+  config.time_zone = 'Eastern Time (US & Canada)'
 end
