c64f6f8309de28299e68269d602e725cef57b760
Kevin Chang <kevin.w.chang@gmail.com>
2015-12-05 21:48:47 -0500

    Update deprecated config option

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module WatchTheSkiesDashboard
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
     config.time_zone = 'Eastern Time (US & Canada)'
     config.active_record.default_timezone = :local
-    config.serve_static_assets = true
+    config.serve_static_files = true
   end
 end
