f578ca6fe7d57adeaa7cc7c248ccc37e0c0cc4c6
Michael Pelletier <michael@societyofgrownups.com>
2016-01-13 11:14:00 -0500

    Remove old RoundProgress plugin for previous terror tracker.

--- app/assets/javascripts/angular/controllers/dashboard.js
+++ app/assets/javascripts/angular/controllers/dashboard.js
@@ -1,6 +1,6 @@
 var dashboardApp = angular.module('dashboardApp', ['dashboardController']);
 
-var dashboardController = angular.module('dashboardController', ['timer', 'angular-svg-round-progress']);
+var dashboardController = angular.module('dashboardController', ['timer']);
 
 dashboardController.controller('DashboardCtrl', ['$rootScope', '$scope', '$http', '$interval',
   function($rootScope, $scope, $http, $interval) {
