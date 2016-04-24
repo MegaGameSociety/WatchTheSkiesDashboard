dcfe18e7f8c56b0485e5ba12d6dc8c203c475081
Kevin Chang <kevin.w.chang@gmail.com>
2016-04-24 11:39:14 -0400

    Fix auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,20 +4,28 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def authenticate_control!
-    unless current_user.control?
+    user = current_user
+    redirect_to root_path if user.nil?
+
+    unless user.control?
       redirect_to root_path
     end
   end
 
   def authenticate_super_admin!
-    unless (current_user.super_admin?)
+    user = current_user
+    redirect_to root_path if user.nil?
+
+    unless (user.super_admin?)
       redirect_to root_path
     end
   end
 
   def authenticate_admin!
-    unless (current_user.admin?)
-      if current_user.control?
+    user = current_user
+    redirect_to root_path if user.nil?
+    unless (user.admin?)
+      if user.control?
         redirect_to admin_control_path
       else
         redirect_to root_path
