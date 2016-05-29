11140fbe7fae725df3380da135dcd34327cbb635
Michael Pelletier <michael@societyofgrownups.com>
2016-05-29 09:30:24 -0400

    Add message filtering.

--- app/assets/javascripts/angular/controllers/mobile-dashboard.js
+++ app/assets/javascripts/angular/controllers/mobile-dashboard.js
@@ -207,4 +207,26 @@ angular.module('mobileApp', ['timer', 'truncate'])
     $scope.selectedMessage = null;
   }
 
+  // Message Filtering
+  $scope.filterOptions = [
+    {"id": 0, "title": "Show all messsages"},
+    {"id": 1, "title": "Show those waiting on me"},
+    {"id": 2, "title": "Show those I am waiting on"}
+  ]
+  $scope.messageFilter = $scope.filterOptions[0];
+
+  $scope.filterFn = function(conversation) {
+    var filter = $scope.messageFilter.id;
+
+    if (filter === 0) {
+      return true;
+    } else if (filter === 1) {
+      return conversation.messages[conversation.messages.length - 1].sender !== $scope.myCountry;
+    } else if (filter === 2) {
+      return conversation.messages[conversation.messages.length - 1].sender === $scope.myCountry;
+    } else {
+      return true;
+    }
+  }
+
 }]);
