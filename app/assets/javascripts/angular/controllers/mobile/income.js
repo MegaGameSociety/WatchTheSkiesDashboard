087a43881ecb421710d4fbc6464b61ed24c23fdd
Michael Pelletier <michael@societyofgrownups.com>
2016-06-14 08:45:29 -0400

    Remove filter from Income.

--- app/assets/javascripts/angular/controllers/mobile/income.js
+++ app/assets/javascripts/angular/controllers/mobile/income.js
@@ -1,46 +1,5 @@
 (function() {
   angular.module('wtsApp').controller('incomeCtrl', ['$scope', '$http', '$interval', function($scope, $http, $interval) {
-
-    var getRounds = function() {
-      var allRounds = [];
-
-      // This is for filtering by round.
-      for (var i = 0; i <= $scope.round; i++) {
-        allRounds.push(i);
-      }
-
-      return allRounds;
-    }
-
-    var setRounds = function() {
-      $scope.allRounds = getRounds();
-      $scope.selectedRound = $scope.round;
-    }
-
-    setRounds();
-
-    $scope.$on('base_data_changed', function() {
-      setRounds();
-    })
-
-    $scope.updateFilter = function() {
-      filterPr($scope.selectedRound);
-    }
-
-    var filterPr = function(round) {
-      $scope.filtered_pr = $.map($scope.pr, function(relations) {
-        // These come back as strings from the template.
-        round = parseInt(round);
-
-        if ((relations.team_id === $scope.myCountryId)
-        && (relations.round === round)) {
-          return relations;
-        }  else {
-          return null;
-        }
-      });
-    }
-
     var apiCall = function() {
       $http.get('/api/income_data').then(
       function successCallback(response) {
@@ -48,34 +7,11 @@
 
         var result = response['data']['result'];
 
-        // Reserves Array
-        var reserves = result['reserves'];
-        var total_reserves = _.reduce(reserves, function(total, reserve_item) {
-          if (reserve_item.round === $scope.round) {
-            return total += reserve_item.amount;
-          }
-          return total;
-        }, 0);
-
-        // PR Array
-        var pr = result['pr'];
-
-        // Total PR
-        var total_pr = _.reduce(pr, function(total, pr_item) {
-          if (pr_item.round === $scope.round) {
-            return total += pr_item.pr_amount;
-          }
-          return total;
-        }, 0);
-
-        $scope.pr = pr;
-        $scope.totalPr = total_pr;
-        $scope.reserves = total_reserves;
+        $scope.pr = result['pr'];
+        $scope.totalPr = result['total_pr'];
+        $scope.reserves = result['reserves'];
         $scope.incomeLevel = result['income_level'];
         $scope.incomeValue = result['income_value'];
-
-        $scope.updateFilter();
-
       },
       function errorCallback(response) {
         $('#connection-error').show();
