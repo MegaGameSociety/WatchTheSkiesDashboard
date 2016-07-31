d7bc9d4c87e32e6297bacaaee96850adf763d2e8
Kevin Chang <kevin.w.chang@gmail.com>
2016-07-31 15:27:21 -0400

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Create User
-u = User.create(role: "SuperAdmin")
-u.email = "wts@wts.com"
-u.password='swordfish'
-u.password_confirmation='swordfish'
-u.role = 'SuperAdmin'
-u.save
+u = User.create({
+        role: "SuperAdmin",
+        email: "wts@wts.com",
+        password: 'swordfish',
+        password_confirmation: 'swordfish'
+})
 
 # Create the user's game
 u.game= Game.create(
@@ -35,49 +35,49 @@ game = u.game
 
 #An example message for the database. Probably doesn't need to exist in the final version.
 # m = Message.create(
-# 	sender: "UK",
-# 	recipient: "France",
-# 	content: "This is an example message",
-# 	round_number: 0,
-# 	game_id: g.id
+#   sender: "UK",
+#   recipient: "France",
+#   content: "This is an example message",
+#   round_number: 0,
+#   game_id: g.id
 # )
 
 # create initial Terror Item
 game.terror_trackers.push(TerrorTracker.create(
-  description: "Initial Terror",
-  amount: 0,
-  round: game.round
-  ))
+                            description: "Initial Terror",
+                            amount: 0,
+                            round: game.round
+))
 
 # Create some news message stuff
 game.news_messages.push(NewsMessage.create(
-  title: "Daily Earth News reports:",
-  content: "DEN reported some things",
-  game: game,
-  round: game.round,
-  visible_content:true,
-  visible_image:true,
-  media_url: "http://megagamesociety.com/images/mainlogo.png",
-  media_landscape: false
-  ))
+      title: "Daily Earth News reports:",
+      content: "DEN reported some things",
+      game: game,
+      round: game.round,
+      visible_content:true,
+      visible_image:true,
+      media_url: "http://megagamesociety.com/images/mainlogo.png",
+      media_landscape: false
+))
 
 game.news_messages.push(NewsMessage.create(
-  title: "Global News Network reports:",
-  content: "GNN also reported some things.",
-  game: game,
-  round: game.round,
-  visible_content: true,
-  visible_image: true
-  ))
+      title: "Global News Network reports:",
+      content: "GNN also reported some things.",
+      game: game,
+      round: game.round,
+      visible_content: true,
+      visible_image: true
+))
 
 game.news_messages.push(NewsMessage.create(
-  title: "Science & Financial Times reports:",
-  content: "Science & Financial Times reported some stuff.",
-  game: game,
-  round: game.round,
-  visible_content:true,
-  visible_image: true
-  ))
+      title: "Science & Financial Times reports:",
+      content: "Science & Financial Times reported some stuff.",
+      game: game,
+      round: game.round,
+      visible_content:true,
+      visible_image: true
+))
 
 
 # Insert Team Roles
@@ -124,7 +124,7 @@ TeamRole.create(
 ).save()
 
 Game::COUNTRIES.each do |country|
-  team = Team.create(team_name: country)
+  team = Team.find_or_create_by(team_name: country)
   #Income starts at 6.
   income = Income.create(game: game, round: game.round, amount: 6)
 
